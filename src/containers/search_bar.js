4b8e7a7400cd3e53ac64ab2b4de909bf981e9765
puan0601 <anton.pugach@gmail.com>
2018-05-26 14:01:08 -0700

    adds button to SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,7 +4,10 @@ export default class SearchBar extends Component {
     render() {
         return (
             <form className="input-group">
-                <input />
+            <input />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
             </form>
         );
     }
