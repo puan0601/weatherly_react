03f89e24e3f3ecc2f114ba31fa6a269ff7f100fc
puan0601 <anton.pugach@gmail.com>
2018-05-26 17:01:35 -0700

    adds AXIOS and openweather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = 'b40ce07e5cfece4d1f6c5233ee88d75b';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    };
+}
