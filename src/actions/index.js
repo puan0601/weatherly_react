9c9ce623680729fb12ff074e92971bb01a10b0f6
puan0601 <anton.pugach@gmail.com>
2018-05-29 15:37:04 -0700

    binds fetchWeather action creator and connects to SearchBar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'b40ce07e5cfece4d1f6c5233ee88d75b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
