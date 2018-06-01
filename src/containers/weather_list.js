094346f0d8be4fe5ff6df5dd7b1fca8625465a08
puan0601 <anton.pugach@gmail.com>
2018-06-01 14:53:15 -0700

    creates weather_list container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                    </tr>
+                </thead>
+            </table>
+        );
+    }
+}
