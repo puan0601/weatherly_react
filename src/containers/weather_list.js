9e5d32acf1736f1f79879e5f8bd53382827a6056
puan0601 <anton.pugach@gmail.com>
2018-06-01 15:02:28 -0700

    connects weatherlist to redux

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
     render() {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
             </table>
         );
     }
 }
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
