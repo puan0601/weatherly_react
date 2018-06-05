1ba1c95956654631e4dcb7e4a57c456c8cdacb2a
puan0601 <anton.pugach@gmail.com>
2018-06-05 14:50:08 -0700

    adds charts for pressures, humidities

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,7 +4,7 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default (props) => {
     return (
         <div>
-            <Sparklines height={120} width={180} data={props.data}>
+            <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
                 <SparklinesLine color={props.color} />
             </Sparklines>
         </div>
