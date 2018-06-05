5c78ec37bb3f5958c60a69b49c49ffbfd1a8e2a7
puan0601 <anton.pugach@gmail.com>
2018-06-05 14:53:32 -0700

    adds reference-line avg to chart comp

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
 
 export default (props) => {
     return (
         <div>
             <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
                 <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
             </Sparklines>
         </div>
     );
